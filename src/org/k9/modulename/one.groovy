e35bb7c7ffb3a0d1cc0171580b98e7278c7b5aa9
Prakash.S <prakash.s@Prakashs-MP.local>
2018-02-03 01:41:14 +0530

    for extended shared library

---
+++ src/org/k9/modulename/one.groovy
@@ -0,0 +1,20 @@
+package org.k9.first
+
+class One implements Serializable {
+  def config
+  def script
+
+  One(script,config) {
+    this.config = config
+    this.script = script
+  }
+
+  
+  void wish()
+  {
+	  this.script.stage('wish'){
+		this.script.sh('echo Hello ' + this.config.name)
+	}	
+  }
+
+}
