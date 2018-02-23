7c1dc2fbb20c80aa0cd9bb703f69fe265a803def
Prakash Singh <psingh.singh361@gmail.com>
2018-02-24 01:04:24 +0530

    Update ArtifactStorage.groovy

--- src/org/k9/storagetest/ArtifactStorage.groovy
+++ src/org/k9/storagetest/ArtifactStorage.groovy
@@ -66,9 +66,10 @@ class ArtifactStorage implements Serializable {
 			this.script.echo "${this.script.env.WORKSPACE}"
 			//def file1 = new File("/tmp/workspace/new/target/spring-boot-rest-example-0.5.0.war")
         		def response = client.post(path: "/upload/storage/v1/b/"+this.config.bucket+"/o",
-   		     		query: [uploadType: 'media', name: 'spring-boot-web-jsp-1.0-222.war'],
-				body: [file: new File("/home/psingh_singh361/test/spring-boot-web-jsp-1.0.war")],
-       				headers: [Authorization: 'Bearer '+ this.config.accesstoken, "Content-Type" : "application/java-archive"]
+   		     		query: [uploadType: 'media', name: 'output-222.png'],
+	//			body: [file: new File("/home/psingh_singh361/test/spring-boot-web-jsp-1.0.war")],
+	                        body: [file: new File("/home/psingh_singh361/test/output.png")],
+       				headers: [Authorization: 'Bearer '+ this.config.accesstoken, "Content-Type" : "image/png"]
         			)
 			
 			this.script.echo "4"
